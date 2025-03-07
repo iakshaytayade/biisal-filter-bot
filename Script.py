b8c5443fb667523b071e5f5659d050d27bae4c9e
iakshaytayade <akshaytayade525@gmail.com>
2025-03-07 17:06:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -107,9 +107,9 @@ User - {}"""
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = """<a href='https://t.me/PeloFlix'><b>{file_name}</b></a> 
+    FILE_CAPTION = """<a href='https://t.me/PeloFlix'><b>{file_name}</b> 
     
- Provided By - <a href='https://t.me/PeloFlix'><b>PeloFlix</a></a>"""
+ Provided By - </b> href='https://t.me/PeloFlix'><b>PeloFlix</b>"""
     
     RESTART_TXT = """<b>
 📅 Dᴀᴛᴇ : <code>{}</code>
