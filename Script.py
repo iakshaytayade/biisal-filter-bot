cfe197c687f01c99c5e794fbfd68ee2ab1e58b7b
iakshaytayade <akshaytayade525@gmail.com>
2025-03-07 18:59:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TXT = """<b> my name is #PeloFlix. i can provide kind of content to our members without any cost. just send movie name and get his file... {} {},"""
+    START_TXT = """<a><b> my name is #PeloFlix. i can provide kind of content to our members without any cost. just send movie name and get his file..."""
     
     HELP_TXT = """<b>ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴꜱ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ᴅᴏᴄᴜᴍᴇɴᴛᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ꜱᴘᴇᴄɪꜰɪᴄ ᴍᴏᴅᴜʟᴇꜱ..</b>"""
     
